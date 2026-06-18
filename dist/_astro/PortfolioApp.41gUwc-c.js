9020ae0673a024946119f490bd6c62b1685ef1f9
Cooper Nusbaum <coopernusbaum@yahoo.com>
2026-06-18 16:45:32 -0500

    fix: place portfolio tagline beside name

---
+++ dist/_astro/PortfolioApp.41gUwc-c.js
@@ -0,0 +1,1 @@
+import{p as t}from"./PortfolioApp.CZk6vk4X.js";import"./index.UEuQJ2Tp.js";export{t as default};
--- dist/_astro/PortfolioApp.Dh40yWFP.js
+++
@@ -1,1 +0,0 @@
-import{p as t}from"./PortfolioApp.j1byknbf.js";import"./index.UEuQJ2Tp.js";export{t as default};
